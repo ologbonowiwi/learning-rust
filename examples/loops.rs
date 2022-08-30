5671b0bf2b6edd180e8fda2b4bb4fe7cd28e7c0f
Wesley Matos <wm4tos.777@gmail.com>
2022-08-29 21:03:05 -0300

    use for loop

--- examples/loops.rs
+++ examples/loops.rs
@@ -29,10 +29,26 @@ fn with_loop(multiplier: u8) {
     }
 }
 
+fn with_for(multiplier: u8) {
+    for counter in 1..11 {
+        // for counter in 1..=10 will give the same result
+        println!(
+            "{} * {} = {}",
+            counter,
+            multiplier,
+            // forcing the conversion because counter is i32 by default
+            counter as i16 * i16::from(multiplier)
+        );
+    }
+}
+
 fn main() {
     println!("----------------------- While -----------------------");
     with_while(255);
 
     println!("----------------------- Loop ----------------------- ");
     with_loop(255);
+
+    println!("----------------------- For ----------------------- ");
+    with_for(255);
 }
