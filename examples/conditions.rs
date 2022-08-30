19733ac07a647941126d2f13acbbeb039ee4bf73
Wesley Matos <wm4tos.777@gmail.com>
2022-08-29 21:11:39 -0300

    learn more about match statement

--- examples/conditions.rs
+++ examples/conditions.rs
@@ -1,5 +1,4 @@
-fn main() {
-    let age: u8 = 18;
+fn check_age(age: u8) {
     let responsible_give_authorization: bool = true;
     let is_of_age = age >= 18;
 
@@ -15,4 +14,22 @@ fn main() {
     let condition = if is_of_age { "of age" } else { "minor" };
 
     println!("Is {} person", condition);
+}
+
+fn check_language(language: &str) {
+    let purpose = match language {
+        "PHP" => "Web",
+        "Kotlin" => "Android",
+        "Python" => "Data Science",
+        _ => "Uncovered",
+    };
+
+    println!("The {} purpose is {}", language, purpose);
+}
+
+fn main() {
+    check_age(18);
+    check_language("PHP");
+    check_language("Kotlin");
+    check_language("Python");
 }
