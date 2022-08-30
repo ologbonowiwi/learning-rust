8360867faf9ab7873e205023582e65164996612d
Wesley Matos <wm4tos.777@gmail.com>
2022-08-30 10:44:06 -0300

    borrow common and mutable values

--- examples/ownership.rs
+++ examples/ownership.rs
@@ -1,4 +1,4 @@
-fn main() {
+fn ownership() {
     let random_string = String::from("Wesley");
     steal(random_string);
 
@@ -9,5 +9,41 @@ fn main() {
 }
 
 fn steal(string: String) {
-    println!("{}", string);
+    println!("steal: {}", string);
+}
+
+fn ownership_with_borrow() {
+    let random_string = String::from("Wesley");
+
+    // Now I will pass not the value (with the ownership) of this variable, but I will pass a REFERENCE of random_string
+    // And the ownership will remain on this scope
+    borrow(&random_string);
+
+    println!("ownership_with_borrow: {}", random_string);
+}
+
+fn borrow(string: &String) {
+    println!("borrow: {}", string);
+}
+
+fn ownership_with_borrow_and_mutability() {
+    let mut random_string = String::from("Wesley");
+
+    // I need to pass the value with the "mut" annotation
+    // The variable passed as reference need to be mutable too
+    borrow_and_change(&mut random_string);
+
+    println!("ownership_with_borrow_and_mutability: {}", random_string);
+}
+
+// The param need to receive the mut annotation to guarantee that this value is mutable
+fn borrow_and_change(string: &mut String) {
+    string.push_str(" Matos");
+    println!("borrow_and_change: {}", string);
+}
+
+fn main() {
+    ownership();
+    ownership_with_borrow();
+    ownership_with_borrow_and_mutability();
 }
